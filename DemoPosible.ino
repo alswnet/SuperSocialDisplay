2f61b1c5aea4a2429afc1b38f49db8c4448a35c9
chepecarlos <chepecarlos@alswblog.org>
2018-02-27 17:05:37 -0600

    Agregando el display

--- DemoPosible.ino
+++ DemoPosible.ino
@@ -1,4 +1,3 @@
-
 #include <YoutubeApi.h>
 #include "InstagramStats.h"
 #include <ESP8266WiFi.h>
@@ -6,13 +5,11 @@
 #include <ArduinoJson.h>
 #include <FacebookApi.h>
 
-
 #include "JsonStreamingParser.h"
 char ssid[] = "TURBONETT_ALSW";       // your network SSID (name)
 char password[] = "2526-4897";  // your network key
 #define API_KEY "AIzaSyChaTNrG24ikJ2D8hAtL2LUTPgk8U-MSyg"  // your google apps API Token
 #define CHANNEL_ID "UCS5yb75qx5GFOG-uV5JLYlQ" // makes up the url of channel
-
 
 WiFiClientSecure client;
 InstagramStats instaStats(client);
@@ -29,17 +26,19 @@ unsigned long whenDueToCheck = 0;
 
 String userName = "alswnet";
 
+byte segmentClock = 13;
+byte segmentLatch = 12;
+byte segmentData = 16;
+
 void setup() {
 
   Serial.begin(115200);
 
-  // Set WiFi to station mode and disconnect from an AP if it was Previously
-  // connected
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(100);
 
-  // Attempt to connect to Wifi network:
+  Serial.println("Iniciando Programa");
   Serial.print("Connecting Wifi: ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -54,6 +53,14 @@ void setup() {
   Serial.println(ip);
 
   apifb = new FacebookApi(client, FACEBOOK_ACCESS_TOKEN, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET);
+
+  pinMode(segmentClock, OUTPUT);
+  pinMode(segmentData, OUTPUT);
+  pinMode(segmentLatch, OUTPUT);
+
+  digitalWrite(segmentClock, LOW);
+  digitalWrite(segmentData, LOW);
+  digitalWrite(segmentLatch, LOW);
 
 }
 
@@ -72,8 +79,8 @@ void getYoutube() {
 
 void getFacebook() {
   int pageLikes = apifb->getPageFanCount("163069780414846");
-    Serial.print("Facebook: ");
-    Serial.println(pageLikes);
+  Serial.print("Facebook: ");
+  Serial.println(pageLikes);
 }
 
 void loop() {
@@ -83,6 +90,77 @@ void loop() {
     getInstagramStatsForUser();
     getYoutube();
     getFacebook();
+    showNumber(10);
     whenDueToCheck = timeNow + delayBetweenChecks;
+  }
+}
+
+//Takes a number and displays 2 numbers. Displays absolute value (no negatives)
+void showNumber(float value)
+{
+  int number = abs(value); //Remove negative signs and any decimals
+
+  //Serial.print("number: ");
+  //Serial.println(number);
+
+  for (byte x = 0 ; x < 2 ; x++)
+  {
+    int remainder = number % 10;
+
+    postNumber(remainder, false);
+
+    number /= 10;
+  }
+
+  //Latch the current segment data
+  digitalWrite(segmentLatch, LOW);
+  digitalWrite(segmentLatch, HIGH); //Register moves storage register on the rising edge of RCK
+}
+
+//Given a number, or '-', shifts it out to the display
+void postNumber(byte number, boolean decimal)
+{
+  //    -  A
+  //   / / F/B
+  //    -  G
+  //   / / E/C
+  //    -. D/DP
+
+#define a  1<<0
+#define b  1<<6
+#define c  1<<5
+#define d  1<<4
+#define e  1<<3
+#define f  1<<1
+#define g  1<<2
+#define dp 1<<7
+
+  byte segments;
+
+  switch (number)
+  {
+    case 1: segments = b | c; break;
+    case 2: segments = a | b | d | e | g; break;
+    case 3: segments = a | b | c | d | g; break;
+    case 4: segments = f | g | b | c; break;
+    case 5: segments = a | f | g | c | d; break;
+    case 6: segments = a | f | g | e | c | d; break;
+    case 7: segments = a | b | c; break;
+    case 8: segments = a | b | c | d | e | f | g; break;
+    case 9: segments = a | b | c | d | f | g; break;
+    case 0: segments = a | b | c | d | e | f; break;
+    case ' ': segments = 0; break;
+    case 'c': segments = g | e | d; break;
+    case '-': segments = g; break;
+  }
+
+  if (decimal) segments |= dp;
+
+  //Clock these bits out to the drivers
+  for (byte x = 0 ; x < 8 ; x++)
+  {
+    digitalWrite(segmentClock, LOW);
+    digitalWrite(segmentData, segments & 1 << (7 - x));
+    digitalWrite(segmentClock, HIGH); //Data transfers to the register on the rising edge of SRCK
   }
 }

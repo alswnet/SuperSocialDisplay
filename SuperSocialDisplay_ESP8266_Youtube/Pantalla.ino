ba39f9c3709b25bb1ba1d9b207b488812e32fbaf
chepecarlos <chepecarlos@alswblog.org>
2018-11-13 14:10:47 -0600

    Codigo de displat con nueva animacion

---
+++ SuperSocialDisplay_ESP8266_Youtube/Pantalla.ino
@@ -0,0 +1,90 @@
+
+byte segmentClock = 13;//
+byte segmentLatch = 12;//
+byte segmentData = 16;//
+int ValocidadBarrido = 500;
+
+void InicializarPantallas() {
+  pinMode(segmentClock, OUTPUT);
+  pinMode(segmentData, OUTPUT);
+  pinMode(segmentLatch, OUTPUT);
+
+  digitalWrite(segmentClock, LOW);
+  digitalWrite(segmentData, LOW);
+  digitalWrite(segmentLatch, LOW);
+
+}
+
+void MostarRandom(int Digitos) {
+  int Numero = random(pow(10, Digitos));
+  MostarNumero(Numero, Digitos);
+}
+
+void MostarBarrido(float Valor, int Digitos) {
+  MostarNumero( 0, Digitos);
+  int Divisor = pow(10, Digitos);
+  for (int i = 0; i <= Digitos; i++) {
+    delay(ValocidadBarrido);
+    MostarNumero(Valor / Divisor, Digitos);
+    Divisor /= 10;
+  }
+}
+
+
+void MostarNumero(float Valor, int Digitos) {
+  int number = abs(Valor);
+  for (byte x = 0 ; x < Digitos ; x++)  {
+    int remainder = number % 10;
+    if (number == 0) {
+      postNumber(' ', false);
+    }
+    else {
+      postNumber(remainder, false);
+    }
+    number /= 10;
+  }
+
+  digitalWrite(segmentLatch, LOW);
+  digitalWrite(segmentLatch, HIGH);
+}
+
+void postNumber(byte number, boolean decimal) {
+
+  //     --     A
+  //   /    /   F/B
+  //     --     G
+  //   /    /   E/C
+  //     --     D/DP
+
+#define a  1<<0
+#define b  1<<6
+#define c  1<<5
+#define d  1<<4
+#define e  1<<3
+#define f  1<<1
+#define g  1<<2
+#define dp 1<<7
+
+  byte segments;
+  switch (number)  {
+    case 1: segments = b | c; break;
+    case 2: segments = a | b | d | e | g; break;
+    case 3: segments = a | b | c | d | g; break;
+    case 4: segments = f | g | b | c; break;
+    case 5: segments = a | f | g | c | d; break;
+    case 6: segments = a | f | g | e | c | d; break;
+    case 7: segments = a | b | c; break;
+    case 8: segments = a | b | c | d | e | f | g; break;
+    case 9: segments = a | b | c | d | f | g; break;
+    case 0: segments = a | b | c | d | e | f; break;
+    case ' ': segments = 0; break;
+    case 'c': segments = g | e | d; break;
+    case '-': segments = g; break;
+  }
+  if (decimal) segments |= dp;
+  for (byte x = 0 ; x < 8 ; x++) {
+    digitalWrite(segmentClock, LOW);
+    digitalWrite(segmentData, segments & 1 << (7 - x));
+    digitalWrite(segmentClock, HIGH);
+  }
+}
---
+++ SuperSocialDisplay_ESP8266_Youtube/SuperSocialDisplay_ESP8266_Youtube.ino
@@ -0,0 +1,133 @@
+#include <YoutubeApi.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiClientSecure.h>
+
+#include <ArduinoJson.h> // This Sketch doesn't technically need this, but the library does so it must be installed.
+
+ESP8266WiFiMulti wifiMulti;
+
+char ssid1[] = "ALSW2";       // your network SSID (name)
+char password1[] = "7210-3607";  // your network key
+char ssid2[] = "Garcia Wifi";
+char password2[] = "cirugia93";
+#define API_KEY "AIzaSyChaTNrG24ikJ2D8hAtL2LUTPgk8U-MSyg"  // your google apps API Token
+#define CHANNEL_ID "UCS5yb75qx5GFOG-uV5JLYlQ" // makes up the url of channel
+
+
+WiFiClientSecure client;
+YoutubeApi api(API_KEY, client);
+
+unsigned long api_mtbs = 5000; //mean time between api requests
+unsigned long api_lasttime;   //last time api request has been done
+int CantidadSub  = 0;
+unsigned long TiempoBajada = 0;
+long subs = 0;
+
+void setup() {
+
+  Serial.begin(115200);
+  InicializarPantallas();
+  MostarNumero(1234, 4);
+
+  WiFi.mode(WIFI_STA);
+  //WiFi.disconnect();
+  delay(100);
+
+  Serial.println("Conectando con Wifi: ");
+
+  wifiMulti.addAP(ssid1, password1);
+  wifiMulti.addAP(ssid2, password2);
+
+
+  if (wifiMulti.run() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+
+}
+
+void loop() {
+
+  while (wifiMulti.run() != WL_CONNECTED) {
+    Serial.println("Sin Wifi!");
+    MostarNumero(1234, 4);
+    delay(500);
+    MostarNumero(8888, 4);
+    delay(500);
+    MostarNumero(0, 4);
+  }
+
+  if (millis() - api_lasttime > api_mtbs)  {
+    if (api.getChannelStatistics(CHANNEL_ID))    {
+      int CantidadSubNueva = api.channelStats.subscriberCount;
+
+      if (CantidadSubNueva > CantidadSub) {
+        AnimarSub(CantidadSubNueva);
+        CantidadSub = CantidadSubNueva;
+        TiempoBajada = 0;
+      } else if (CantidadSubNueva < CantidadSub) {
+        if (TiempoBajada == 0) {
+          Serial.println("Bajando Contador");
+          TiempoBajada = millis();
+        } else if (TiempoBajada + 1000 * 90 < millis() ) {
+          Serial.println("Bajando Listo");
+          TiempoBajada = 0;
+          MostarNumero(CantidadSubNueva, 4);
+          CantidadSub =  CantidadSubNueva;
+        }
+      }
+      Serial.println("---------Stats---------");
+      Serial.print("Subscriber Count: ");
+      Serial.println(api.channelStats.subscriberCount);
+      Serial.print("View Count: ");
+      Serial.println(api.channelStats.viewCount);
+      //Serial.print("Comment Count: ");
+      //Serial.println(api.channelStats.commentCount);
+      Serial.print("Video Count: ");
+      Serial.println(api.channelStats.videoCount);
+      // Probably not needed :)
+      //Serial.print("hiddenSubscriberCount: ");
+      //Serial.println(api.channelStats.hiddenSubscriberCount);
+      Serial.println("------------------------");
+
+    }
+    api_lasttime = millis();
+  }
+}
+
+void AnimarSub(int CantidadAnimar) {
+  int tmp = 0;
+  int Espera = 1000;
+  int Minimo = 1;
+
+  while (tmp < CantidadAnimar) {
+
+    MostarNumero(tmp, 4);
+    tmp++;
+    delay(Espera);
+
+    if (tmp + 5  > CantidadAnimar) {
+      Espera += 100;
+    }
+    else {
+      Espera -= 100;
+    }
+    if (Espera < Minimo) {
+      Espera = Minimo;
+    }
+  }
+  int R = random(10);
+  Serial.println(R);
+  if (R > 7) {
+    MostarNumero(CantidadAnimar + 1, 4);
+    delay(Espera);
+    MostarNumero(CantidadAnimar + 2, 4);
+    delay(Espera);
+    MostarNumero(CantidadAnimar + 1, 4);
+    delay(Espera);
+  }
+  MostarNumero(CantidadAnimar, 4);
+}
